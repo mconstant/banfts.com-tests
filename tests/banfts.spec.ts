101877a682571effb271e789b6ce5f6894030f72
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-06 13:45:30 +0000

    remove title test

--- tests/banfts.spec.ts
+++ tests/banfts.spec.ts
@@ -1,12 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://banfts.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/banfts\.com/);
-});
-
 test('get started link', async ({ page }) => {
   await page.goto('https://banfts.com/');
 
